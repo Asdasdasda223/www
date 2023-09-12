cf28ab0e376a0a5af46871ac190012ae0da74335
Kreya <32719454+Tsukreya@users.noreply.github.com>
2023-09-12 16:36:00 +0000

    Create dl.sh

---
+++ dl.sh
@@ -0,0 +1,2 @@
+!wget https://huggingface.co/Aotsuyu/Gishiki/resolve/main/Gishikiv1.1.safetensors -O /content/wat/models/Stable-diffusion/
+!wget https://huggingface.co/Aotsuyu/Gishiki/resolve/main/Gishikiv1.1.safetensors -O /content/wat/models/VAE
