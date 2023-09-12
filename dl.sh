0f36bc7380946fcef66c712e98c4e1ab08d2b14b
Kreya <32719454+Tsukreya@users.noreply.github.com>
2023-09-12 16:44:12 +0000

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -1,2 +1,2 @@
-wget https://huggingface.co/Aotsuyu/Gishiki/resolve/main/Gishikiv1.1.safetensors -O /content/wat/models/Stable-diffusion/
-wget https://huggingface.co/Aotsuyu/Gishiki/resolve/main/Gishikiv1.1.safetensors -O /content/wat/models/VAE
+wget https://huggingface.co/Aotsuyu/Gishiki/resolve/main/Gishikiv1.1.safetensors -P /content/wat/models/Stable-diffusion/
+wget https://huggingface.co/Aotsuyu/Gishiki/resolve/main/Gishikiv1.1.safetensors -P /content/wat/models/VAE
