c99f1e6f8fd4a767440458a070b9386f62496ff3
Kreya <32719454+Tsukreya@users.noreply.github.com>
2023-09-12 16:48:13 +0000

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -1,2 +1,2 @@
 wget https://huggingface.co/Aotsuyu/Gishiki/resolve/main/Gishikiv1.1.safetensors -P /content/wat/models/Stable-diffusion/
-wget https://huggingface.co/Aotsuyu/Gishiki/resolve/main/Gishikiv1.1.safetensors -P /content/wat/models/VAE
+wget https://huggingface.co/hakurei/waifu-diffusion-v1-4/resolve/main/vae/kl-f8-anime2.ckpt -P /content/wat/models/VAE
